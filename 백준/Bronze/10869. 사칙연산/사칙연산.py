125c03acf489d30787c6206f179440f953da987b
mindorrri <minseo21118@gmail.com>
2025-07-05 19:49:18 +0900

    [Bronze V] Title: 사칙연산, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,10 @@
+input_number = input().split()
+
+number1 = int(input_number[0])
+number2 = int(input_number[1])
+
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 // number2)
+print(number1 % number2)
